c1d43d20e74c85a2d0186c16bfd9200a244fbf54
phoenixdantehanren <1454526863@qq.com>
2018-01-05 00:01:44 +0800

    91

--- 91. Decode Ways/源.cpp
+++ 91. Decode Ways/源.cpp
@@ -11,50 +11,73 @@ public:
 		int n = s.length();
 		if (n == 0)
 			return 0;
-		vector<vector<int>> dp(n);
-		for (int i = 0; i != n; ++i)
-		{
-			vector<int> temp(n, 0);
-			dp[i] = temp;
-		}
-		for (int i = n - 1; i >= 0; --i)
-		{
-			if (s[i] == '0' && (i == 0 || s[i - 1] != '1'))
-			{
-				return 0;
-			}
-			for (int j = i; j < n; ++j)
-			{
-				if (i == j)
-				{
-					if (s[i] == '0')
-					{
-						dp[i][j] = 0;
-					}
-					else
-					{
-						dp[i][j] = 1;
-					}			
-					//++count;
-				}
-				else
-				{
-					string sub_str = s.substr(j - 1, 2);
-					char decode_c = stoi(sub_str) - 1 + 'A';
-					if (decode_c >= 'A' && decode_c <= 'Z' && sub_str != "01" && sub_str != "10")
-					{
-						dp[i][j] = min(dp[i + 1][j], dp[i][j - 1]) + 1;		
-					}
-					else
-					{
-						dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
-					}
-				}
 
+		vector<int> dp(n + 1, 0);
+		dp[0] = 1;
+		dp[1] = s[0] != '0' ? 1 : 0;
+		for (int i = 2; i <= n; ++i)
+		{
+			int first = stoi(s.substr(i - 1, 1));
+			int second = stoi(s.substr(i - 2, 2));
+			if (first >= 1 && first <= 9)
+			{
+				dp[i] += dp[i - 1];
+			}
+			if (second >= 10 && second <= 26)
+			{
+				dp[i] += dp[i - 2];
 			}
 		}
-		return dp[0][n - 1];
+		return dp[n];
 	}
+	//int numDecodings(string s) {
+	//	int n = s.length();
+	//	if (n == 0)
+	//		return 0;
+	//	vector<vector<int>> dp(n);
+	//	for (int i = 0; i != n; ++i)
+	//	{
+	//		vector<int> temp(n, 0);
+	//		dp[i] = temp;
+	//	}
+	//	for (int i = n - 1; i >= 0; --i)
+	//	{
+	//		if (s[i] == '0' && (i == 0 || s[i - 1] != '1'))
+	//		{
+	//			return 0;
+	//		}
+	//		for (int j = i; j < n; ++j)
+	//		{
+	//			if (i == j)
+	//			{
+	//				if (s[i] == '0')
+	//				{
+	//					dp[i][j] = 0;
+	//				}
+	//				else
+	//				{
+	//					dp[i][j] = 1;
+	//				}			
+	//				//++count;
+	//			}
+	//			else
+	//			{
+	//				string sub_str = s.substr(j - 1, 2);
+	//				char decode_c = stoi(sub_str) - 1 + 'A';
+	//				if (decode_c >= 'A' && decode_c <= 'Z' && sub_str != "01" && sub_str != "10")
+	//				{
+	//					dp[i][j] = min(dp[i + 1][j], dp[i][j - 1]) + 1;		
+	//				}
+	//				else
+	//				{
+	//					dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+	//				}
+	//			}
+
+	//		}
+	//	}
+	//	return dp[0][n - 1];
+	//}
 
 
 
